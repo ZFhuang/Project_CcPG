989c7e434c61a481824f5edd37b49a3cef3d4f5b
Zifeng <huangsnake@gmail.com>
2019-04-08 17:42:38 +0800

    添加项目文件。

---
+++ Debug.win32/Resources/Map/testPlatform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="warTileset_32x32" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="testPlatform.png" trans="ffffff" width="256" height="256"/>
+</tileset>
